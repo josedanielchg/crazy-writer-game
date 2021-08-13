bd142dce091d41464b29bb728837c48d23a19694
Jose Chacon <j.daniel.chg@gmail.com>
2021-08-13 00:29:41 -0400

    feat: - first commits - Estructure.

---
+++ desktop/src/com/crazy_writer_game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.crazy_writer_game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.crazy_writer_game.CrazyWriterGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrazyWriterGame(), config);
+	}
+}
