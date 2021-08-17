c2420beb1f41897e4b8902bf892e13290837e445
Pedro Soto <pedrosotto717@gmail.com>
2021-08-17 19:44:10 -0400

    [fix] read and write into data.json
    Finalized GameOverScreen::class
    Finalized LevelCompletedScreen::class

--- core/src/components/DataLevelsGame.java
+++ core/src/components/DataLevelsGame.java
@@ -5,29 +5,31 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonWriter;
-import com.badlogic.gdx.utils.SerializationException;
-
-import java.io.FileNotFoundException;
 
 public class DataLevelsGame {
     public static Array<GameLevel> listGameLevel;
 
     public static void load() {
+
         try{
             Json json = new Json();
-            FileHandle from;
-            from = Gdx.files.internal("data.json");
 
-            if(Gdx.files.local("data_new.json").exists())
-                from = Gdx.files.internal("data_new.json");
+            if(!Gdx.files.local("data_new.json").exists()){
+                listGameLevel = json.fromJson(Array.class, GameLevel.class, Gdx.files.internal("data.json"));
 
-            listGameLevel = json.fromJson(Array.class, GameLevel.class, from);
-            from.copyTo(Gdx.files.local("data_new.json"));
+                Gdx.files.internal("data.json").copyTo(Gdx.files.local("data_new.json"));
+            }else{
+                FileHandle file = Gdx.files.local("data_new.json");
+                if(!file.readString().isEmpty())
+                    listGameLevel = json.fromJson(Array.class, GameLevel.class, file);
+                else{
+                    listGameLevel = json.fromJson(Array.class, GameLevel.class, Gdx.files.internal("data.json"));
+                    Gdx.files.internal("data.json").copyTo(Gdx.files.local("data_new.json"));
+                }
+            }
+
         }catch (NullPointerException e){
-            System.out.println("FALLO Y PASO AL CATCH");
-            Json js = new Json();
-            listGameLevel = js.fromJson(Array.class, GameLevel.class, Gdx.files.internal("data.json"));
-            Gdx.files.local("data_new.json").copyTo( Gdx.files.local("data_new.json") );
+            System.out.println("ERROR LOAD FILE");
         }
     }
 
